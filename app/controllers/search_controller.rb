0909bb68ce0ece0e386f1e61a60e0724e6ff6181
Hylton <goregehylton@yahoo.com>
2023-06-26 20:31:05 -0500

    Search Logic fixed

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,5 +2,11 @@ class SearchController < ApplicationController
   def index
     @searchterm = params[:query]
     @choosencuisine = params[:cuisine]
+
+    if params[:cuisine] == "All"
+      @restaurants = Restaurant.where("LOWER(RestaurantName) LIKE LOWER(?)", "%#{Restaurant.sanitize_sql_like(params[:query])}%")
+    else
+      @restaurants = Restaurant.where("LOWER(RestaurantName) LIKE LOWER(?) AND LOWER(Cuisine) = LOWER(?)", "%#{Restaurant.sanitize_sql_like(params[:query])}%", params[:cuisine])
+    end
   end
 end
