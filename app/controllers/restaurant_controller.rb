0bd39a9cdf4460963b03d89e7511fdc1ea3420ae
Hylton <goregehylton@yahoo.com>
2023-06-26 19:43:42 -0500

    Added Restaurant Controller and View

---
+++ app/controllers/restaurant_controller.rb
@@ -0,0 +1,9 @@
+class RestaurantController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'chef/index', to: "chef#index"
-  get 'chef/:id', to: "chef#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  oot "chef#index"
+  root "chef#index"
   get "/about", to:"about#index"
+  get 'chef/index', to: "chef#index"
+  get 'chef/:id', to: "chef#show"
+  get 'restaurant/index', to:"restaurant#index"
+  get 'restaurant/show', to:"restaurant#show"
 end
