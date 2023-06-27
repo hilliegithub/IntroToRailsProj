9fdf917dbab69b0682fd260c3e25cdd17dbf5823
Hylton <goregehylton@yahoo.com>
2023-06-26 20:49:46 -0500

    Updated Search Results Page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,8 +5,12 @@ class SearchController < ApplicationController
 
     if params[:cuisine] == "All"
       @restaurants = Restaurant.where("LOWER(RestaurantName) LIKE LOWER(?)", "%#{Restaurant.sanitize_sql_like(params[:query])}%")
+      @count = @restaurants.count
+      @restaurants = @restaurants.page(params[:page])
     else
       @restaurants = Restaurant.where("LOWER(RestaurantName) LIKE LOWER(?) AND LOWER(Cuisine) = LOWER(?)", "%#{Restaurant.sanitize_sql_like(params[:query])}%", params[:cuisine])
+      @count = @restaurants.count
     end
+    @restaurants = @restaurants.page(params[:page])
   end
 end
